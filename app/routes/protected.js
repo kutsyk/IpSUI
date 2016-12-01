84aad8cb3b35ce15d78c1e1d0a70e382895eb50f
kutsyk <vasya.kutsyk@gmail.com>
2016-12-02 00:56:55 +0200

    thtrow shit away

--- app/routes/protected.js
+++
@@ -1,5 +0,0 @@
-import Ember from 'ember';
-import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
-
-export default Ember.Route.extend(AuthenticatedRouteMixin, {
-});
