2262687087ff85d4b37fdf2ed3caee1541e5b7b3
kutsyk <vasya.kutsyk@gmail.com>
2017-02-26 20:35:19 +0200

    #3 add pagination

--- app/controllers/SearchController.js
+++ app/controllers/SearchController.js
@@ -8,23 +8,25 @@ module.exports = function (app) {
     });
 
     app.get('/search', function (req, res) {
+        let page = req.query.page ? req.query.page : 0;
+        let query = req.query.query;
+        let start = page * 10 - 10;
+        let size = 10;
         client.search({
-            q: req.query.query
+            q: query
         }).then(function (body) {
             let hits = body.hits.hits;
             let total = body.hits.total;
-            // mongodb.collection('ips_dev', function (err, collection) {
-            //     collection.count(function (err, count) {
-            //         res.render('search.ejs', {
-            //             count: count
-            //         });
-            //     });
-            // });
+            let pageCount = Math.floor(total / size);
+            pageCount = total % 10 == 0 ? pageCount : pageCount + 1;
             res.render('search.ejs', {
                 user: req.user,
+                query: query,
                 total: total,
-                hits: hits
-            }); // load the index.ejs file
+                hits: hits,
+                pageCount: pageCount,
+                currentPage: page
+            });
         }, function (error) {
             console.trace(error.message);
         });
